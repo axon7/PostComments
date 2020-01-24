f7e1383aba71027ebba5e03e9061bef1a767b18c
Kuba <kubaz9991@gmail.com>
2020-01-24 10:38:53 +0100

    Style comments component and add home button

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,23 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
-
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Title from "./Title";
+import Button from "./Button";
+
+const StyledComment = styled.div`
+  background-color: #ffffff;
+  margin-bottom: 10px;
+  padding: 10px;
+  box-shadow: 0 8px 6px -6px grey;
+  border-radius: 5px;
+  max-width: 960px;
+  max-width: 960px;
+  width: 80%;
+`;
+
 const Comments = ({ comments, loading }) => {
   const commentsToRender = comments.map(item => (
-    <div key={item.id}>
-      <p>{item.name}</p>
-      <p>{item.email}</p>
+    <StyledComment key={item.id}>
+      <p>Name: {item.name}</p>
+      <p>E-mail: {item.email}</p>
       <p>{item.body}</p>
-    </div>
+    </StyledComment>
   ));
 
   return (
-    <div>
-      <p>Comments</p>
-      <Link to="/">Home</Link>
+    <>
+      <Title>Comments</Title>
+      <Button linkTo="/">Home</Button>
 
       {loading ? "Loading" : commentsToRender}
-    </div>
+    </>
   );
 };
 
